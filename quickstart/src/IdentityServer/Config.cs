eacfafdd3f7ac226207d6083ece4a7314cf30816
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-09-04 17:56:49 -0400

    quickstart 2 - Password Grant

--- quickstart/src/IdentityServer/Config.cs
+++ quickstart/src/IdentityServer/Config.cs
@@ -4,11 +4,31 @@
 
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using IdentityServer4.Test;
 
 namespace IdentityServer
 {
     public static class Config
     {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+        }
+
         public static IEnumerable<IdentityResource> GetIdentityResources()
         {
             return new IdentityResource[]
@@ -36,6 +56,23 @@ namespace IdentityServer
                     // no interactive user, use the clientid/secret for authentication
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
 
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+
+                new Client
+                {
+                    ClientId = "ro.client",
+
+                    // resource owner password grant client
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    
                     // secret for authentication
                     ClientSecrets =
                     {
