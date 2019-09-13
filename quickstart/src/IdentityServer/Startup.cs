50a13ab268b076c904c53c77433a95d8b56e21b3
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-09-13 16:13:10 -0400

    adds x509 generation to sign the tokens against!!!!

--- quickstart/src/IdentityServer/Startup.cs
+++ quickstart/src/IdentityServer/Startup.cs
@@ -13,6 +13,9 @@ using System.Reflection;
 using System.Linq;
 using IdentityServer4.EntityFramework.DbContexts;
 using IdentityServer4.EntityFramework.Mappers;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Security.Cryptography;
 
 namespace IdentityServer
 {
@@ -74,14 +77,16 @@ namespace IdentityServer
                     };
                 });
 
-            if (Environment.IsDevelopment())
-            {
-                builder.AddDeveloperSigningCredential();
-            }
-            else
-            {
-                throw new Exception("need to configure key material");
-            }
+            builder.AddSigningCredential(GenerateSelfSignedServerCert("IdentityServerTokenSig"));
+
+            //if (Environment.IsDevelopment())
+            //{
+            //    builder.AddDeveloperSigningCredential();
+            //}
+            //else
+            //{
+            //    throw new Exception("need to configure key material");
+            //}
         }
 
         public void Configure(IApplicationBuilder app)
@@ -101,6 +106,37 @@ namespace IdentityServer
 
             // uncomment, if you want to add an MVC-based UI
             app.UseMvcWithDefaultRoute();
+        }
+
+        private X509Certificate2 GenerateSelfSignedServerCert(string certificateName)
+        {
+            SubjectAlternativeNameBuilder sanBuilder = new SubjectAlternativeNameBuilder();
+            sanBuilder.AddIpAddress(IPAddress.Loopback);
+            sanBuilder.AddIpAddress(IPAddress.IPv6Loopback);
+            sanBuilder.AddDnsName("localhost");
+            sanBuilder.AddDnsName(System.Environment.MachineName);
+
+            X500DistinguishedName distinguishedName = new X500DistinguishedName($"CN={certificateName}");
+
+            using (RSA rsa = RSA.Create(2048))
+            {
+                var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+                request.CertificateExtensions.Add(
+                    new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
+
+
+                request.CertificateExtensions.Add(
+                   new X509EnhancedKeyUsageExtension(
+                       new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
+
+                request.CertificateExtensions.Add(sanBuilder.Build());
+
+                var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+                certificate.FriendlyName = certificateName;
+
+                return new X509Certificate2(certificate.Export(X509ContentType.Pfx, "WeNeedASaf3rPassword"), "WeNeedASaf3rPassword", X509KeyStorageFlags.MachineKeySet);
+            }
         }
 
         private void InitializeDatabase(IApplicationBuilder app)
